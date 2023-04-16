7a64beb62bae9579985090bbeea4e15df73c399f
pkaratanev <preslavkaratanev@gmail.com>
2023-04-16 21:47:49 +0300

    Implement api auth v1

---
+++ app/Http/Controllers/Auth/MobileAuthenticationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
+
+class MobileAuthenticationController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $this->generateToken($user, $request->device_name);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $this->generateToken($user, $request->device_name);
+    }
+
+    protected function generateToken(User $user, string $deviceName)
+    {
+        return $user->createToken($deviceName)->plainTextToken;
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Test Testov',
+            'email' => 'example@example.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
