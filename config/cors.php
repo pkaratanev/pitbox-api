acc737888fa3cc805411df0beb3feec167dcee98
pkaratanev <47246453+pkaratanev@users.noreply.github.com>
2023-07-19 20:14:06 +0300

    Add some localhosts for cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL')],
+    'allowed_origins' => [env('FRONTEND_URL'), 'http://localhost:8080', 'http://localhost:8100'],
 
     'allowed_origins_patterns' => [],
 
